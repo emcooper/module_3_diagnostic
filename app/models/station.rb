2681e3f1b754280269c03ed61bff424f2d7f9ca5
Ellen Cooper <ellencooper74@gmail.com>
2017-08-30 10:50:32 -0600

    adds additional station attributes to model; second feature test passes

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,8 +1,17 @@
 class Station
-  attr_reader :name
-  
+  attr_reader :name,
+              :address,
+              :fuel_type,
+              :access_time,
+              :distance
+
+
   def initialize(data)
     @name = data[:station_name]
+    @address = data[:street_address]
+    @fuel_type = data[:fuel_type_code]
+    @access_time = data[:access_days_time]
+    @distance = data[:distance]
   end
 
   def self.find_by_zip(zip)

8a915097a6426997659bfbd62a8c1f55ce756421
Ellen Cooper <ellencooper74@gmail.com>
2017-08-30 09:40:47 -0600

    adds search controller and index page, modifies feature test

--- spec/features/user_searches_stations_by_zipcode_spec.rb
+++ spec/features/user_searches_stations_by_zipcode_spec.rb
@@ -1,26 +1,28 @@
 RSpec.feature "User searches stations by zipcode" do
   scenario "they are redirected to the results page with 10 results" do
-    VCR.user_cassette('spec/cassettes/user_searches_by_zipcode') do
+    VCR.use_cassette('spec/cassettes/user_searches_by_zipcode') do
+      visit root_path
+      fill_in "q", with: '80203'
+      click_on "Locate"
 
+      expect(current_url).to include('q=80203')
+      expect(current_path).to eq('/search')
+      expect(page).to have_css(".station", count: 10)
     end
   end
   scenario "they only see electric & propane stations" do
-    VCR.user_cassette('spec/cassettes/user_searches_by_zipcode') do
+    VCR.use_cassette('spec/cassettes/user_searches_by_zipcode') do
 
     end
   end
   scenario "they see name, address, fuel type, distance and access times for each station" do
-    VCR.user_cassette('spec/cassettes/user_searches_by_zipcode') do
+    VCR.use_cassette('spec/cassettes/user_searches_by_zipcode') do
+
+    end
+  end
+  scenario "the results are sorted by distance" do
+    VCR.use_cassette('spec/cassettes/user_searches_by_zipcode') do
 
     end
   end
 end
-
-As a user
-When I visit "/"
-And I fill in the search form with 80203
-And I click "Locate"
-Then I should be on page "/search" with parameters visible in the url
-Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-And the stations should be limited to Electric and Propane
-And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
